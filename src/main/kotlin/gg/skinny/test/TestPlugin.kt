2ab2cabe1cd2466c555ffc20dea4cf19b1cd1d09
borehlol <iiskinnyyt@gmail.com>
2026-04-03 13:08:39 -0700

    initial kotlin plugn setup

---
+++ src/main/kotlin/gg/skinny/test/TestPlugin.kt
@@ -0,0 +1,15 @@
+package gg.skinny.test
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TestPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        println("I am iiSkinny. This is my first plugin.")
+    }
+
+    override fun onDisable() {
+
+    }
+
+}
