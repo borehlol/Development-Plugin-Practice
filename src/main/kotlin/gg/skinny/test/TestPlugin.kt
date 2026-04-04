f6948c9ce9b8cc04f4b8d6eb76a8c75e2cfcb58c
borehlol <iiskinnyyt@gmail.com>
2026-04-04 02:42:57 -0700

    implemented kotlin template to start + did /heal

--- src/main/kotlin/gg/skinny/test/TestPlugin.kt
+++ src/main/kotlin/gg/skinny/test/TestPlugin.kt
@@ -1,18 +1,19 @@
 package gg.skinny.test
 
+import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
-import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerEggThrowEvent
 import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.plugin.java.JavaPlugin
-import java.awt.Component
+
 
 class TestPlugin : JavaPlugin(), Listener {
 
     override fun onEnable() {
         server.pluginManager.registerEvents(this, this)
+        getCommand("heal")?.setExecutor(HealCommand())
     }
 
     @EventHandler
@@ -22,6 +23,6 @@ class TestPlugin : JavaPlugin(), Listener {
 
     @EventHandler
     fun onPlayerEggThrow(event: PlayerEggThrowEvent) {
-        event.player.sendMessage(net.kyori.adventure.text.Component.text("u threw a mf egg or sum shit!").color(NamedTextColor.AQUA))
+        event.player.sendMessage(Component.text("u threw a mf egg or sum shit!").color(NamedTextColor.AQUA))
     }
 }
