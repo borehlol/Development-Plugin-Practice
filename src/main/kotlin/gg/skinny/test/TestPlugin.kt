4e466a3ba256bc5dfb36521c5b60cf6f861c641e
borehlol <iiskinnyyt@gmail.com>
2026-04-22 23:29:55 -0700

    added gmc command

--- src/main/kotlin/gg/skinny/test/TestPlugin.kt
+++ src/main/kotlin/gg/skinny/test/TestPlugin.kt
@@ -2,10 +2,10 @@ package gg.skinny.test
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import net.minecraft.gametest.framework.TestCommand
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerEggThrowEvent
-import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 
@@ -14,15 +14,15 @@ class TestPlugin : JavaPlugin(), Listener {
     override fun onEnable() {
         server.pluginManager.registerEvents(this, this)
         getCommand("heal")?.setExecutor(HealCommand())
+        getCommand("gmc")?.setExecutor(GmcCommand())
+        getCommand("gms")?.setExecutor(GmsCommand())
     }
 
-    @EventHandler
-    fun onPlayerMove(event: PlayerMoveEvent) {
-        event.isCancelled = true
-    }
 
     @EventHandler
     fun onPlayerEggThrow(event: PlayerEggThrowEvent) {
         event.player.sendMessage(Component.text("u threw a mf egg or sum shit!").color(NamedTextColor.AQUA))
     }
+
+
 }
